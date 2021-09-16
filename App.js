03a0d6d4d8dacb5a455bee45216b0f2b866e83b0
kehindeBankole <bankolek1@gmail.com>
2021-09-16 23:39:50 +0100

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Landing from './screens/Landing'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Products from './screens/Products';
+
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+//     <View style={styles.container}>
+//  <Landing/>
+//       
+//     </View>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+      <Stack.Navigator  screenOptions={{
+          headerShown: false,
+        }}>
+      <Stack.Screen name="Home" component={Landing} />
+      <Stack.Screen name="Product" component={Products} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Landing.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+export default function Landing({navigation}) {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+  if (!fontsLoaded) {
+    return <Text>loading</Text>;
+  }else{
+    return (
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Image
+            source={require("../assets/images/logo.png")}
+            style={styles.logoText}
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.nextButton}
+          onPress={() => navigation.navigate("Product")}
+        >
+          <Text style={styles.nextButtonText}>next</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: Dimensions.get("screen").height,
+    width: Dimensions.get("screen").width,
+    backgroundColor: "#010035",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 16,
+  },
+  logo: {
+    width: 132,
+    height: 132,
+    borderRadius: 100,
+    backgroundColor: "#FF6E4E",
+    position: "relative",
+  },
+  logoText: {
+    position: "absolute",
+    top: 38,
+    left: 38,
+  },
+  nextButton: {
+    backgroundColor: "#FF6E4E",
+    width: "100%",
+    height: 50,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    position: "absolute",
+    top: Dimensions.get("screen").height - 150,
+  },
+  nextButtonText: {
+    color: "#FFFFFF",
+    fontFamily: "Inter_900Black",
+    fontSize: 16,
+    textTransform: "capitalize",
+  },
+});
